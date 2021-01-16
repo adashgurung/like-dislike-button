cf93a097a573f2fe977f47801f12f828a3c43d41
Adash Gurung <adashleo@outlook.com>
2021-01-16 11:57:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
-/* import FavoriteIcon from "@material-ui/icons/Favorite"; */
+
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 
